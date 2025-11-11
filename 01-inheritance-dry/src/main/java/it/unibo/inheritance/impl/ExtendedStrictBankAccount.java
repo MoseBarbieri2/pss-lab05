9aa9772a2cd1f916ca294ff16840cd741f8d4236
Mosè Barbieri <mose.barbieri@studio.unibo.it>
2025-11-11 09:19:44 +0100

    initialized the ExtendedStrictBankAccount with constructor

---
+++ 01-inheritance-dry/src/main/java/it/unibo/inheritance/impl/ExtendedStrictBankAccount.java
@@ -0,0 +1,8 @@
+package it.unibo.inheritance.impl;
+
+public class ExtendedStrictBankAccount extends SimpleBankAccount{
+    public ExtendedStrictBankAccount(int id, double balance) {
+        super(id, balance);
+    }
+
+}
