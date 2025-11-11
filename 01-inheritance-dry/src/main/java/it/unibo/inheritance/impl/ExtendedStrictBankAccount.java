80d8e9bbac1b2e6d1c12487a2be756b51b8747c6
Mosè Barbieri <mose.barbieri@studio.unibo.it>
2025-11-11 10:12:49 +0100

    Added the TRANSACTION_FEE and overrided the changeManagementFees to work with the new calculation with TRANSACTION_FEE

--- 01-inheritance-dry/src/main/java/it/unibo/inheritance/impl/ExtendedStrictBankAccount.java
+++ 01-inheritance-dry/src/main/java/it/unibo/inheritance/impl/ExtendedStrictBankAccount.java
@@ -1,17 +1,34 @@
 package it.unibo.inheritance.impl;
 
 public class ExtendedStrictBankAccount extends SimpleBankAccount{
+    private static final double TRANSACTION_FEE = 0.1;
+
     public ExtendedStrictBankAccount(int id, double balance) {
         super(id, balance);
     }
     private boolean isWithdrawAllowed(final double amount) {
         return getBalance() >= amount;
     }
-
     @Override
     public void withdraw(final int id, final double amount) {
         if (isWithdrawAllowed(amount)) {
-            withdraw(id, -amount);
+            super.withdraw(id, -amount);
+        }
+    }
+    private void transactionOp(final int id, final double amount) {
+        if (checkUser(id)) {
+            double balanceStrict = getBalance();
+            setBalance(balanceStrict + amount);
+            this.incrementTransactions();
+        }
+    }
+    @Override
+    public void chargeManagementFees(final int id) {
+        final double feeAmount = MANAGEMENT_FEE + getTransactionsCount() * ExtendedStrictBankAccount.TRANSACTION_FEE;
+        if (checkUser(id) && isWithdrawAllowed(feeAmount)) {
+            double balanceStrict = getBalance();
+            setBalance(balanceStrict- feeAmount);
+            resetTransactions();
         }
     }
 }
