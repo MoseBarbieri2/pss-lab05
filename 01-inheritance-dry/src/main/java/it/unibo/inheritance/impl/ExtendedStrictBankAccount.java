9776c0df0e45e93425863905a8d780a27d9a980d
Mosè Barbieri <mose.barbieri@studio.unibo.it>
2025-11-11 09:49:45 +0100

    Added 2 methodos to ExtendedStrictBankAccount: isWithdrawAllowed and did the ovverride of withdraw

--- 01-inheritance-dry/src/main/java/it/unibo/inheritance/impl/ExtendedStrictBankAccount.java
+++ 01-inheritance-dry/src/main/java/it/unibo/inheritance/impl/ExtendedStrictBankAccount.java
@@ -4,5 +4,14 @@ public class ExtendedStrictBankAccount extends SimpleBankAccount{
     public ExtendedStrictBankAccount(int id, double balance) {
         super(id, balance);
     }
+    private boolean isWithdrawAllowed(final double amount) {
+        return getBalance() >= amount;
+    }
 
+    @Override
+    public void withdraw(final int id, final double amount) {
+        if (isWithdrawAllowed(amount)) {
+            withdraw(id, -amount);
+        }
+    }
 }
